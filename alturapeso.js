21193021a2b5073afd9b7e6087019f276ccb306f
Luiz Felipe Santos <111072221+LordLuizFelipe@users.noreply.github.com>
2023-03-10 19:51:37 -0300

    Update alturapeso.js

--- alturapeso.js
+++ alturapeso.js
@@ -1,44 +1,44 @@
-let altura, sexo, totalMasculino = 0, qtdFeminino = 0, somaAlturaMasculino = 0, maiorAltura = 0, menorAltura = Infinity;
+var altura
+var sexo
+var totalMasculino = 0
+var totalFeminino = 0
+var somaAlturaMasculino = 0
+var mediaAlturaMasculino = 0
+var maiorAltura = 0
+var menorAltura = 0
 
-for (let i = 1; i <= 15; i++) {
-  altura = parseFloat(prompt(`Informe a altura da ${i}ª pessoa (em metros):`));
-  if (isNaN(altura) || altura <= 0) {
-    alert("Altura inválida! Tente novamente.");
-    i--;
-    continue;
-  }
+for(var i = 1; i <= 15; i++) {
+    altura = parseFloat(prompt("Informe a altura da "+i+"ª pessoa:"));
+    sexo = prompt("Informe o sexo da "+i+"ª pessoa (M para Masculino e F para Feminino):");
+    if (sexo == "M") {
+        totalMasculino++ ;
+        somaAlturaMasculino += altura;
+    }
 
-  sexo = prompt(`Informe o sexo da ${i}ª pessoa (M para Masculino ou F para Feminino):`);
-  if (sexo.toUpperCase() !== "M" && sexo.toUpperCase() !== "F") {
-    alert("Sexo inválido! Tente novamente.");
-    i--;
-    continue;
-  }
 
-  if (altura > maiorAltura) {
-    maiorAltura = altura;
-  }
+    else totalFeminino++
 
-  if (altura < menorAltura) {
-    menorAltura = altura;
-  }
-
-  if (sexo.toUpperCase() === "M") {
-    totalMasculino++;
-    somaAlturaMasculino += altura;
-  } else {
-    qtdFeminino++;
-  }
+    if (altura > maiorAltura) {
+        maiorAltura = altura;
+    }
+    else if (menorAltura == 0){
+        menorAltura = altura
+    }
+    
+    if (altura < menorAltura){
+        menorAltura = altura
+    }
 }
 
-alert(`Maior altura informada: ${maiorAltura}m`);
-alert(`Menor altura informada: ${menorAltura}m`);
+
+
+
+alert("Maior altura informada: "+maiorAltura+" m");
+alert("Menor altura informada: "+menorAltura+" m");
 
 if (totalMasculino > 0) {
-  const mediaAlturaMasculino = somaAlturaMasculino / totalMasculino;
-  alert(`Média de altura dos homens: ${mediaAlturaMasculino.toFixed(2)}m`);
-} else {
-  alert("Nenhum homem informado.");
+    mediaAlturaMasculino = somaAlturaMasculino / totalMasculino;
+    alert("Média de altura dos homens: "+mediaAlturaMasculino+" m");
 }
 
-alert(`Número de mulheres que responderam: ${qtdFeminino}`);
+alert("Número de mulheres que responderam: "+totalFeminino+" ");
